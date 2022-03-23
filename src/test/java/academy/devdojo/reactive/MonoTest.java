4d1d2aeb9f4e634983cac26a8e626e67cda81cb6
Levi-Ferreira95 <levi_ferreira@external.mckinsey.com>
2022-03-23 12:46:01 -0300

    MonoTest improvements

--- src/test/java/academy/devdojo/reactive/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/MonoTest.java
@@ -2,7 +2,6 @@ package academy.devdojo.reactive;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscription;
 
@@ -10,22 +9,22 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @Slf4j
-/*
-  Reactive Streams
-  1. Asynchronous
-  2. Non-blocking
-  3. Backpressure
-  Publisher <- (subscribe) Subscriber
-  Subscription is created
-  Publisher (onSubscribe with the subscription) -> Subscriber
-  Subscription <- (request N) Subscriber
-  Publisher -> (onNext) Subscriber
-  until:
-  1. Publisher sends all the objects requested.
-  2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
-  3. There is an error. (onError) -> subscriber and subscription will be canceled
- */
 public class MonoTest {
+
+    /*
+    *   Reactive Streams
+    *   1. Asynchronous
+    *   2. Non-blocking
+    *   3. Backpressure
+    *   Publisher <- (subscribe) Subscriber
+    *   Subscription is created
+    *   Publisher (onSubscribe with the subscription) -> Subscriber
+    *   Subscription <- (request N) Subscriber
+    *   Publisher -> (onNext) Subscriber
+    *   1. Publisher sends all the objects requested.
+    *   2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled
+    *   3. There is an error. (onError) -> subscriber and subscription will be canceled
+    * */
 
     @Test
     void monoSubscriber() {
